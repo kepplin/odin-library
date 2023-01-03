5b2518b191b2eefb58a55a9dcf04e37630424d13
Pranav Borse <pranavborse91@gmail.com>
2023-01-03 14:43:40 +1100

    Fix bug, books created with button weren't getting styled

--- bookshelf.js
+++ bookshelf.js
@@ -9,37 +9,63 @@ function getRandomInt(min, max) {
 function randomChoice(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
+function addBookToLibrary() {
+  // Creating book elements
+  let book = document.createElement("div");
+  let spine = document.createElement("div");
+  let spineTitle = document.createElement("span");
+  let spineAuthor = document.createElement("span");
+  let top = document.createElement("div");
+  let cover = document.createElement("div");
 
-let spines = Object.values(document.getElementsByClassName("spine"));
-let covers = Object.values(document.getElementsByClassName("cover"));
-let tops = Object.values(document.getElementsByClassName("top"));
+  // Add classes to each book component
+  book.classList.add("book");
+  spine.classList.add("side", "spine");
+  spineTitle.classList.add("spine-title");
+  spineAuthor.classList.add("spine-author");
+  top.classList.add("side", "top");
+  cover.classList.add("side", "cover");
 
-let availablePatterns = getRootCssStyles();
+  // bookShelf > book > (spine > spineTitle + spineAuthor) + top + cover
+  bookShelf.appendChild(book);
+  book.appendChild(spine);
+  book.appendChild(top);
+  book.appendChild(cover);
+  spine.appendChild(spineTitle);
+  spine.appendChild(spineAuthor);
 
-let availableColors = [
-  "maroon",
-  "darkgreen",
-  "darkolivegreen",
-  "brown",
-  "saddlebrown",
-  "sienna",
-  "midnightblue",
-];
+  let spines = Object.values(document.querySelectorAll(".spine")); // GETS IT FROM THE HTML FILE HOW DO WE GET IT FROM THE JS FILE??
+  let covers = Object.values(document.querySelectorAll(".cover"));
+  let tops = Object.values(document.querySelectorAll(".top"));
 
-// assign a random height, pattern and colour to each book
-spines.map(function (s, i) {
-  let randomHeight = getRandomInt(220, 290);
-  s.style.height = `${randomHeight}px`;
-  s.style.top = `${280 - randomHeight}px`;
+  let availablePatterns = getRootCssStyles();
 
-  let randomPattern = randomChoice(availablePatterns);
-  s.style.backgroundImage = `var(${randomPattern})`;
+  let availableColors = [
+    "maroon",
+    "darkgreen",
+    "darkolivegreen",
+    "brown",
+    "saddlebrown",
+    "sienna",
+    "midnightblue",
+  ];
 
-  let randomColor = randomChoice(availableColors);
-  s.style.backgroundColor = randomColor;
+  // assign a random height, pattern and colour to each book
+  spines.map(function (s, i) {
+    let randomHeight = getRandomInt(220, 290);
+    s.style.height = `${randomHeight}px`;
+    s.style.top = `${280 - randomHeight}px`;
 
-  covers[i].style.height = `${randomHeight}px`;
-  covers[i].style.top = `${280 - randomHeight}px`;
+    let randomPattern = randomChoice(availablePatterns);
+    s.style.backgroundImage = `var(${randomPattern})`;
 
-  tops[i].style.top = `${280 - randomHeight}px`;
-});
+    let randomColor = randomChoice(availableColors);
+    s.style.backgroundColor = randomColor;
+
+    covers[i].style.height = `${randomHeight}px`;
+    covers[i].style.top = `${280 - randomHeight}px`;
+
+    tops[i].style.top = `${280 - randomHeight}px`;
+  });
+}
+addButton.addEventListener("click", addBookToLibrary);
--- script.js
+++ script.js
@@ -9,31 +9,3 @@ function Book(title, author, pages, readStatus) {
   this.pages = pages;
   this.readStatus = readStatus;
 }
-
-function addBookToLibrary() {
-  // Creating book elements
-  let book = document.createElement("div");
-  let spine = document.createElement("div");
-  let spineTitle = document.createElement("span");
-  let spineAuthor = document.createElement("span");
-  let top = document.createElement("div");
-  let cover = document.createElement("div");
-
-  // Add classes to each book component
-  book.classList.add("book");
-  spine.classList.add("side", "spine");
-  spineTitle.classList.add("spine-title");
-  spineAuthor.classList.add("spine-author");
-  top.classList.add("side", "top");
-  cover.classList.add("side", "cover");
-
-  // bookShelf > book > (spine > spineTitle + spineAuthor) + top + cover
-  bookShelf.appendChild(book);
-  book.appendChild(spine);
-  book.appendChild(top);
-  book.appendChild(cover);
-  spine.appendChild(spineTitle);
-  spine.appendChild(spineAuthor);
-}
-
-addButton.addEventListener("click", addBookToLibrary);
