9629d4f7e4d8aadbf89c2b6e4d01930e119e5efc
Pranav Borse <pranavborse91@gmail.com>
2023-01-04 14:14:06 +1100

    Create popup function for form

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const addButton = document.querySelector(".addButton");
 const bookShelf = document.querySelector(".bookshelf");
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -9,3 +12,34 @@ function Book(title, author, pages, readStatus) {
 }
 function displayForm() {}
 addButton.addEventListener("click", displayForm);
+
+// Pop up form
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
