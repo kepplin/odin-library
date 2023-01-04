468f9d1de886909bf6dde5d5926ef411917727ab
Pranav Borse <pranavborse91@gmail.com>
2023-01-04 13:47:32 +1100

    Create modal

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const addButton = document.querySelector(".addButton");
 const bookShelf = document.querySelector(".bookshelf");
 
-let myLibrary = [];
-
 function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus;
 }
+function displayForm() {}
+addButton.addEventListener("click", displayForm);
